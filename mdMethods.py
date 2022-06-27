1391c9793fa0ccef3537187ca5a0161deb78b443
Previo Prakasa <previo.prakasa@gmail.com>
2022-06-27 16:52:46 +0700

    redo: py2md.py

--- mdMethods.py
+++ mdMethods.py
@@ -1,175 +1,192 @@
-## todo
-    # [ ] Headings
-    # [ ] Styling text
-    # [ ] Quoting text
-    # [ ] Quoting code
-    # [ ] Links
-    # [?] Section links
-    # [as embed in url] Images
-    # [ ] Lists
-    # [ ] Task lists
-    # [?] Mentioning people and teams
-    # [ ] Paragraphs
-    # [ ] Footnotes
-    # [ ] Comments
-    # [ ] Ignoring Markdown formatting
-    # [ ] diagrams
-    # [ ] table
+class MDParser:
 
-# https://github.com/jincheng9/markdown_supported_languages#for-markdown-texts-we-need-to-specify-the-languages-for-corresponding-syntax-highlighting
-md_const_code_syntaxes = ['Cucumber', 'abap', 'ada', 'ahk', 'apacheconf', 'applescript', 
-    'as', 'as3', 'asy', 'bash', 'bat', 'befunge', 'blitzmax', 'boo', 'brainfuck', 'c', 
-    'cfm', 'cheetah', 'cl', 'clojure', 'cmake', 'coffeescript', 'console', 'control', 
-    'cpp', 'csharp', 'css', 'cython', 'd', 'delphi', 'diff', 'dpatch', 'duel', 'dylan', 
-    'erb', 'erl', 'erlang', 'evoque', 'factor', 'felix', 'fortran', 'gas', 'genshi', 
-    'glsl', 'gnuplot', 'go', 'groff', 'haml', 'haskell', 'html', 'hx', 'hybris', 'ini', 
-    'io', 'ioke', 'irc', 'jade', 'java', 'js', 'jsp', 'lhs', 'llvm', 'logtalk', 'lua', 
-    'make', 'mako', 'maql', 'mason', 'markdown', 'modelica', 'modula2', 'moocode', 'mupad', 
-    'mxml', 'myghty', 'nasm', 'newspeak', 'objdump', 'objectivec', 'objectivej', 'ocaml', 
-    'ooc', 'perl', 'php', 'postscript', 'pot', 'pov', 'prolog', 'properties', 'protobuf', 
-    'py3tb', 'pytb', 'python', 'r', 'rb', 'rconsole', 'rebol', 'redcode', 'rhtml', 'rst', 
-    'sass', 'scala', 'scaml', 'scheme', 'scss', 'smalltalk', 'smarty', 'sourceslist', 'splus', 
-    'sql', 'sqlite3', 'squidconf', 'ssp', 'tcl', 'tcsh', 'tex', 'text', 'v', 'vala', 'vbnet', 
-    'velocity', 'vim', 'xml', 'xquery', 'xslt', 'yaml'
-    ]
+    ## todo
+        # [x] Headings
+        # [x] Styling text
+        # [x] Quoting text
+        # [x] Quoting code
+        # [x] Links
+        # [?] Section links
+        # [as embed in url] Images
+        # [x] Lists
+        # [x] Task lists
+        # [?] Mentioning people and teams
+        # [x] Paragraphs
+        # [x] Footnotes
+        # [x] Comments
+        # [x] Ignoring Markdown formatting
+        # [x] diagrams
+        # [x] table
 
-md_const_codediagram_syntaxes = ['mermaid', 'geoJSON', 'topoJSON', 'ASCII STL']
+    # https://github.com/jincheng9/markdown_supported_languages
+    code_syntaxes = ['Cucumber', 'abap', 'ada', 'ahk', 'apacheconf', 'applescript', 
+        'as', 'as3', 'asy', 'bash', 'bat', 'befunge', 'blitzmax', 'boo', 'brainfuck', 'c', 
+        'cfm', 'cheetah', 'cl', 'clojure', 'cmake', 'coffeescript', 'console', 'control', 
+        'cpp', 'csharp', 'css', 'cython', 'd', 'delphi', 'diff', 'dpatch', 'duel', 'dylan', 
+        'erb', 'erl', 'erlang', 'evoque', 'factor', 'felix', 'fortran', 'gas', 'genshi', 
+        'glsl', 'gnuplot', 'go', 'groff', 'haml', 'haskell', 'html', 'hx', 'hybris', 'ini', 
+        'io', 'ioke', 'irc', 'jade', 'java', 'js', 'jsp', 'lhs', 'llvm', 'logtalk', 'lua', 
+        'make', 'mako', 'maql', 'mason', 'markdown', 'modelica', 'modula2', 'moocode', 'mupad', 
+        'mxml', 'myghty', 'nasm', 'newspeak', 'objdump', 'objectivec', 'objectivej', 'ocaml', 
+        'ooc', 'perl', 'php', 'postscript', 'pot', 'pov', 'prolog', 'properties', 'protobuf', 
+        'py3tb', 'pytb', 'python', 'r', 'rb', 'rconsole', 'rebol', 'redcode', 'rhtml', 'rst', 
+        'sass', 'scala', 'scaml', 'scheme', 'scss', 'smalltalk', 'smarty', 'sourceslist', 'splus', 
+        'sql', 'sqlite3', 'squidconf', 'ssp', 'tcl', 'tcsh', 'tex', 'text', 'v', 'vala', 'vbnet', 
+        'velocity', 'vim', 'xml', 'xquery', 'xslt', 'yaml'
+        ]
 
-md_const_styiling_formats = {
-    'i': '*',
-    'b': '**',
-    'ib': '***',
-    'bi': '***',
-    'st': '~~',
-    'sub': ['<sub>', '</sub>'],
-    'sup': ['<sup>', '</sup>'],
-    # 'sub': '~',
-    # 'sup': '^',
-}
+    codediagram_syntaxes = ['mermaid', 'geoJSON', 'topoJSON', 'ASCII STL']
 
-def md_newline(num=2) -> str:
-    return '\n'*(num + 1)
+    styiling_formats = {
+        'i': '*',
+        'b': '**',
+        'ib': '***',
+        'bi': '***',
+        'st': '~~',
+        'sub': ['<sub>', '</sub>'],
+        'sup': ['<sup>', '</sup>'],
+        # 'sub': '~',
+        # 'sup': '^',
+    }
 
-def md_heading(string:str, level:int) -> str:
-    assert type(level) == int, f'{level=} not an integer'
-    assert level > 0 or level < 6, f'{level=}: heading level cannot be less than 0 or more than equal to 6'
-    return '#'*level + f' {string}'
+    @staticmethod
+    def newline(num=2) -> str:
+        return '\n'*(num + 1)
 
-def md_paragraph(string:str) -> str:
-    string = string.replace('\n', ' ')
-    string = string.split(' ')
-    return ' '.join(i for i in string if i)
+    @staticmethod
+    def heading(string:str, level:int) -> str:
+        assert type(level) == int, f'{level=} not an integer'
+        assert level > 0 or level < 6, f'{level=}: heading level cannot be less than 0 or more than equal to 6'
+        return '#'*level + f' {string}'
 
-def md_quote(string:str, level:int=1) -> str:
-    assert type(level) == int, f'{level=} not an integer'
-    assert level > 0, f'{level=}: level cannot be less than 0'
-    return '>'*level + f' {md_paragraph(string)}'
+    @staticmethod
+    def paragraph(string:str) -> str:
+        string = string.replace('\n', ' ')
+        string = string.split(' ')
+        return ' '.join(i for i in string if i)
 
-def md_codeblock(string:str, syntax:str, override=False) -> str:
-    if syntax not in md_const_code_syntaxes and not override:
-        syntax = ''
-    return f'``` {syntax}\n{string}\n```'
+    @staticmethod
+    def quote(string:str, level:int=1) -> str:
+        assert type(level) == int, f'{level=} not an integer'
+        assert level > 0, f'{level=}: level cannot be less than 0'
+        return '>'*level + f' {md_paragraph(string)}'
 
-def md_diagrams(string:str, syntax:str) -> str:
-    syntax = syntax if syntax in md_const_codediagram_syntaxes else ''
-    return md_codeblock(string, syntax, override=True)
+    @staticmethod
+    def codeblock(string:str, syntax:str, override=False) -> str:
+        if syntax not in code_syntaxes and not override:
+            syntax = ''
+        return f'``` {syntax}\n{string}\n```'
 
-def md_styiling(string:str, style:str) -> str:
-    if style not in md_const_styiling_formats:
-        return string
-    else:
-        style = md_const_styiling_formats[style]
-        if type(style) == list:
-            return f'{style[0]}{string}{style[1]}'
+    @staticmethod
+    def diagrams(string:str, syntax:str) -> str:
+        syntax = syntax if syntax in codediagram_syntaxes else ''
+        return md_codeblock(string, syntax, override=True)
+
+    @staticmethod
+    def styiling(string:str, style:str) -> str:
+        if style not in styiling_formats:
+            return string
         else:
-            return f'{style}{string}{style}'
-
-def md_links(url:str, alttext:str='', embed=False) -> str:
-    if alttext and embed:
-        return f'![{alttext}]({url})'
-    elif alttext:
-        return f'[{alttext}]({url})'
-    else:
-        return url
-
-def md_linkuser(string:str) -> str:
-    return f'@{string}'
-
-def md_comment(string:str) -> str:
-    return f'<!-- {string} -->'
-
-def md_footnote(ref:str, desc:str='') -> str:
-    if desc:
-        return f'[^{ref}]: {desc}'
-    return f'[^{ref}]'
-
-def md_lists(lists, level:int=1, num=False, offset=0) -> str:
-    assert type(level) == int, f'{level=} not an integer'
-    assert level > 0, f'{level=}: heading level cannot be less than 0'
-    level -= 1
-    buffer = []
-    for n, item in enumerate(lists):
-        n += offset 
-        if type(item) == list or type(item) == tuple:
-            buffer.append(md_lists(item, level+2, num))
-        else:
-            if num:
-                string = '   '*level + f'{n+1}. {item}'
+            style = styiling_formats[style]
+            if type(style) == list:
+                return f'{style[0]}{string}{style[1]}'
             else:
-                string = '  '*level + f'- {item}'
-            buffer.append(string)
-    return '\n'.join(buffer)
+                return f'{style}{string}{style}'
 
-def md_tasks(lists, level:int=1) -> str:
-    assert type(level) == int, f'{level=} not an integer'
-    assert level > 0, f'{level=}: heading level cannot be less than 0'
+    @staticmethod
+    def links(url:str, alttext:str='', embed=False) -> str:
+        if alttext and embed:
+            return f'![{alttext}]({url})'
+        elif alttext:
+            return f'[{alttext}]({url})'
+        else:
+            return url
 
-    def p(lists):
+    @staticmethod
+    def linkuser(string:str) -> str:
+        return f'@{string}'
+
+    @staticmethod
+    def comment(string:str) -> str:
+        return f'<!-- {string} -->'
+
+    @staticmethod
+    def footnote(ref:str, desc:str='') -> str:
+        if desc:
+            return f'[^{ref}]: {desc}'
+        return f'[^{ref}]'
+
+    @staticmethod
+    def lists(lists, level:int=1, num=False, offset=0) -> str:
+        assert type(level) == int, f'{level=} not an integer'
+        assert level > 0, f'{level=}: heading level cannot be less than 0'
+        level -= 1
         buffer = []
-        for item in lists:
-            if type(item) == list or type(item) == tuple\
-                and type(item[0]) != bool:
-                buffer.append(p(item))
+        for n, item in enumerate(lists):
+            n += offset 
+            if type(item) == list or type(item) == tuple:
+                buffer.append(md_lists(item, level+2, num))
             else:
-                if type(item) == list or type(item) == tuple:
-                    cond = 'x' if item[0] else ' '
-                    buffer.append(f'[{cond}] {item[1]}')
+                if num:
+                    string = '   '*level + f'{n+1}. {item}'
                 else:
-                    buffer.append(f'[ ] {item}')
-        return buffer
+                    string = '  '*level + f'- {item}'
+                buffer.append(string)
+        return '\n'.join(buffer)
 
-    lists = p(lists)
-    return md_lists(lists, level=level, num=False)
+    @staticmethod
+    def tasks(lists, level:int=1) -> str:
+        assert type(level) == int, f'{level=} not an integer'
+        assert level > 0, f'{level=}: heading level cannot be less than 0'
 
-def md_table_content(lists) -> str:
-    return '| ' + ' | '.join([str(l) for l in lists]) + ' |'
+        def p(lists):
+            buffer = []
+            for item in lists:
+                if type(item) == list or type(item) == tuple\
+                    and type(item[0]) != bool:
+                    buffer.append(p(item))
+                else:
+                    if type(item) == list or type(item) == tuple:
+                        cond = 'x' if item[0] else ' '
+                        buffer.append(f'[{cond}] {item[1]}')
+                    else:
+                        buffer.append(f'[ ] {item}')
+            return buffer
 
-def md_table_header(lists, aligns=None) -> str:
-    if type(aligns) == list or type(aligns) == tuple:
-        if len(aligns) < len(lists):
-            aligns += [None]*(len(lists) - len(aligns))
-    else:
-        aligns = [None] * len(lists)
-    aligns = [a.lower()[0] if type(a) == str else '' for a in aligns]
-    offset = 0
-    for n, align in enumerate(aligns):
-        l = len(lists[n])
-        if l < 3: 
-            l = 3
+        lists = p(lists)
+        return md_lists(lists, level=level, num=False)
 
-        if align == 'r':
-            aligns[n] = '-'*(l-1) + ':'
-        elif align == 'c':
-            aligns[n] = ':'+'-'*(l-2) +':'
-        elif align == 'l':
-            aligns[n] = ':' + '-'*(l-1) 
+    @staticmethod
+    def table_content(lists) -> str:
+        return '| ' + ' | '.join([str(l) for l in lists]) + ' |'
+
+    @staticmethod
+    def table_header(lists, aligns=None) -> str:
+        if type(aligns) == list or type(aligns) == tuple:
+            if len(aligns) < len(lists):
+                aligns += [None]*(len(lists) - len(aligns))
         else:
-            aligns[n] = '-' * l
+            aligns = [None] * len(lists)
+        aligns = [a.lower()[0] if type(a) == str else '' for a in aligns]
+        offset = 0
+        for n, align in enumerate(aligns):
+            l = len(lists[n])
+            if l < 3: 
+                l = 3
+
+            if align == 'r':
+                aligns[n] = '-'*(l-1) + ':'
+            elif align == 'c':
+                aligns[n] = ':'+'-'*(l-2) +':'
+            elif align == 'l':
+                aligns[n] = ':' + '-'*(l-1) 
+            else:
+                aligns[n] = '-' * l
 
 
-    heads = md_table_content(lists)
-    aligns = md_table_content(aligns)
-    return f'{heads}\n{aligns}'
+        heads = md_table_content(lists)
+        aligns = md_table_content(aligns)
+        return f'{heads}\n{aligns}'
 
 
 if __name__ == '__main__':
--- py2md.py
+++ py2md.py
@@ -1,134 +1,133 @@
+
 class Py2MD:
 
-    class TOKEN: 
-        def __init__(self, name, inline, group=None):
+    class __DoubleDeque:
+            def __init__(self):
+                self.buffer = [None, None]
+
+            def __repr__(self):
+                return str(self.buffer)
+
+            def flush(self):
+                buffer = self.buffer
+                self.buffer = [None, None]
+                return buffer
+
+            def put(self, item):
+                self.buffer[0] = self.buffer[-1]
+                self.buffer[-1] = item
+
+            def peek(self, index):
+                return self.buffer[index]
+
+            def peekl(self):
+                return self.peek(1)
+
+            def peekr(self):
+                return self.peek(-1)
+
+    class __TokenContainer:
+        __ids = []
+        __tokens = []
+        __names = []
+
+        def getById(self, ident):
+            index = self.__ids.index(ident)
+            return self.__tokens[index]
+
+        def append_to_container(self, ident, token, name=''):
+            self.__ids.append(ident)
+            self.__tokens.append(token)
+            self.__names.append(name)
+
+        def get_token_ids(self):
+            return self.__ids.copy()
+
+        def get_token_names(self):
+            return self.__names.copy()
+
+    class __Token(__TokenContainer):
+        def __init__(self, ident, name, isinline=False):
+            self.id = ident
             self.name = name
-            self.inline = inline
-            self.group = group
+            self.append_to_container(ident, self, name)
+            self.isinline = isinline
+            self.params = []
 
         def __repr__(self):
-            return f'token::{self.name}'
+            return f'<t::{self.name}>'
 
         def __eq__(self, o):
-            if not self.group:
-                return self.name == o
-            return self.group == o
+            return self.id == o
 
-    T_PARAG = TOKEN('paragraph', True, 'parag')
-    T_HEADINGS = TOKEN('heading', False, 'head')
-    T_TASKS = TOKEN('task', True)
-    T_TABLE = TOKEN('table', True)
-    T_CODEBLOCK = TOKEN('codeblock', True, 'parag')
-    T_FOOTNOTE = TOKEN('footnote', False, 'parag')
-    T_URL = TOKEN('url', True, 'parag')
-    T_DEFINITION = TOKEN('definition', False)
-    T_SUBSCRIPT = TOKEN('subscript', True, 'parag')
-    T_SUPSCRIPT = TOKEN('superscript', True, 'parag')
-    T_HRULE = TOKEN('horizontal_rule', False)
-    T_BREAK = TOKEN('break', False)
+    __T_PARAG      = __Token('p', 'paragraph', True) 
+    __T_HEADINGS   = __Token('h', 'heading')
+    __T_TASKS      = __Token('l', 'list')
+    __T_TASKS      = __Token('ts', 'task')
+    __T_TABLE      = __Token('t', 'table')
+    __T_CODEBLOCK  = __Token('cb', 'codeblock')
+    __T_FOOTNOTE   = __Token('fn', 'footnote', True)
+    __T_URL        = __Token('url', 'url', True)
+    __T_DEFINITION = __Token('d', 'definition')
+    __T_HRULE      = __Token('hr', 'horizontal_rule')
+    __T_BREAK      = __Token('b', 'break')
+    __T_SUBSCRIPT  = __Token('sub', 'subscript', True)
+    __T_SUPSCRIPT  = __Token('sup', 'superscript', True)
+
+    __TOKEN_CONTAINER = __TokenContainer()
 
     def __init__(self):
-        # defaults
-        self.max_characters = 72
-        self.num_newlines = 2
-        # prog init
-        self.__buffer = []
-        self.__TOKENS = {
-            'p': self.T_PARAG, 
-            'h': self.T_HEADINGS,
-            'ts': self.T_TASKS,
-            'tb': self.T_TABLE,
-            'cb': self.T_CODEBLOCK,
-            'fn': self.T_FOOTNOTE,
-            'url': self.T_URL,
-            'def': self.T_DEFINITION,
-            'sb': self.T_SUBSCRIPT,
-            'ss': self.T_SUPSCRIPT,
-            'hr': self.T_HRULE,
-            'b': self.T_BREAK,
-            }
+        self.ddeque = self.__DoubleDeque()
+        self.stream = []
+        self.tags = self.__TOKEN_CONTAINER.get_token_ids()
+        self.tag_names = self.__TOKEN_CONTAINER.get_token_names()
+        self.tags_dict = dict(zip(self.tags, self.tag_names))
+        self.getByID = self.__TOKEN_CONTAINER.getById
 
+    def add(self, tag, value):
+        tagid = tag.rstrip('0123456879')
+        param = tag[len(tagid):]
+        param = param if param else None
 
-
-    def parse(self) -> str:
-        string_buffer = []
-        prev_token = None
-        print(self.__buffer)
-        while self.__buffer:
-            token = self.__buffer.pop(0)
-            param = self.__buffer.pop(0)
-            value = self.__buffer.pop(0)
-            if token.inline and param[1] and token == prev_token:
-                string_buffer[-1] += self.__parse_by_token(value, token, param)
-            else:
-                string_buffer.append(self.__parse_by_token(value, token, param))
-            prev_token = token
-        return ('\n'*self.num_newlines + '\n').join(string_buffer).strip()
-
-    def clear(self):
-        self.__buffer = []
-
-    def add(self, ttype='p', value='', inline=True):
-        # adds item to the buffer stack/deque
-        ident = ttype.rstrip('0123456879')
-        param = ttype[len(ident):]
-        param = int(param) if param else None
-        # print('added: ',ident, param, value)
-        param = (param, inline)
-        token = self.__TOKENS[ident]
-        self.__buffer.append(token)
-        self.__buffer.append(param)
-        self.__buffer.append(value)
+        if tagid not in self.tags:
+            raise ValueError(f'tag \'{tagid}\' is not present for parser.')
+        else:
+            tag_token = self.getByID(tagid)
+            tag_token.params.append(param)
+            self.stream.append(tag_token)
+            self.stream.append(value)
         return self
 
+    def addlist(self, tag, values):
+        for item in values:
+            self.add(tag, item)
+        return self
 
-    def __parse_by_token(self, value, token, param) -> str:
-        previous_group = token.group
-        token.group = None
-        print(f'parsing: {token.name}',param,value)
-        param = param[0] if param[0] else 1
-        if token == self.T_PARAG: 
-            value = value.replace('\n', ' ').split(' ')
-            value = [v for v in value if v]
-            value = ' '.join(value)
-        elif token == self.T_HEADINGS:
-            value = '#' * param + ' ' + value
-        elif token == self.T_TASKS: pass
-        elif token == self.T_TABLE: pass
-        elif token == self.T_CODEBLOCK: pass
-        # elif token == self.T_FOOTNOTE: pass
-        elif token == self.T_URL: pass
-        elif token == self.T_DEFINITION: 
-            value = value[0] + '\n:' + value[1]
-        elif token == self.T_SUBSCRIPT:
-            value = '~' + value + '~'
-        elif token == self.T_SUPSCRIPT:
-            value = '^' + value + '^'
-        elif token == self.T_HRULE: 
-            value = '---'
-        elif token == self.T_BREAK: 
-            value = ''
-        token.group = previous_group
-        return value + ' '
+    def parse(self) -> str:
+        # todo
+        string = ''
+        pbuff = []
+        print(self.stream)
+        param = None
+        while self.stream:
+            value = self.stream.pop(0)
+            if isinstance(value, self.__Token):
+                param = value.params.pop(0)
+
+            print(param)
+            self.ddeque.put(value)
+            print(self.ddeque)
+        self.ddeque.flush()
+        return string
 
 if __name__ == '__main__':
     mdparser = Py2MD()
-    mdparser.num_newlines = 1
-    mdparser.add('h', 'Py2MD')
-    mdparser.add('p', 'python internal to markdown parser.')
-    mdparser.add('p', 'impractical, but fun.')
-    mdparser.add('hr', 'horizontal rules doenst parse value')
-    mdparser.add('h1', 'example heading')
-    mdparser.add('h2', 'example heading number 2')
-    mdparser.add('p', 'example paragraph somehow differs.        ')
-    mdparser.add('p', 'example paragraph and newlinw').add('b')
-    mdparser.add('p', '123')
-    mdparser.add('p', 'foooobar')
-    mdparser.add('ss', 'sup')
-    mdparser.add('sb', 'sub')
-    mdparser.add('def', ('definition', 'this is a definition, though it doesnt work in github markdown format.'))
-    mdparser.add('def', ('definition', 'this is a definition'))
-
-    mdparser.add('p', 'example paragraph that doestn go inline', inline=False)
-    with open('README.md', 'w') as fp:
-        fp.write(mdparser.parse())
+    print(mdparser.tags)
+    mdparser.add('p', 'value')
+    mdparser.addlist('p', ['value2', 'value3'])
+    mdparser.add('h2', 'heading1')
+    mdparser.add('h3', 'heading2')
+    mdparser.add('h4', 'heading3')
+    mdparser.add('p', 'value')
+    mdparser.parse()
+    
